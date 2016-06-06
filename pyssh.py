dcc45dd2a8676c61cb968941b7d2510692fbccc1
tnsy <tokacprzak@gmail.com>
2016-06-06 09:47:05 +0200

    added sort + printing of ip addresses

--- pyssh.py
+++ pyssh.py
@@ -16,18 +16,21 @@ ssh_hosts = {'my_own':['tns', '127.0.0.1'],
 for k in ssh_hosts.keys():
     ssh_list.append(k)
 
+ssh_list = sorted(ssh_list)
+
 print '\nSelect host to ssh to:\n'
 
 #use above list to print out all hosts
 for i in ssh_list:
-    print str(ssh_list_counter) + '. ' + i
+    x = ssh_hosts.get(i)
+    print str(ssh_list_counter) + '. ' + i + ' - ' + x[1]
     ssh_list_counter += 1
 
 index = int(raw_input('\nSelect host to login [0 - %s]: ' % (len(ssh_list) -
                                                            1)))
 
-#create var 'v' that is a value - list - of key (ssh_list[index]) 
-v =  ssh_hosts.get(ssh_list[index]) 
+#create var 'v' that is a value - list - of key (ssh_list[index])
+v =  ssh_hosts.get(ssh_list[index])
 
 #create string for ssh login
 login = 'ssh ' + v[0] + '@' + v[1]
